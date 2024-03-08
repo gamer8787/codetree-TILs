6db5b2465473c05b29e1c2af575b2a2bbe024d07
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-09 02:42:39 +0900

    Title: 루돌프의 반란, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240309/루돌프의 반란/rudolph-rebellion.cpp
+++ 240309/루돌프의 반란/rudolph-rebellion.cpp
@@ -1,396 +1,198 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <cmath>
-
-int dy[8] = { 0,0,1,-1,1,1,-1,-1 };
-int dx[8] = { 1,-1,0,0,1,-1,1,-1 };
-
-int sdy[4] = {-1,0,1,0};
-int sdx[4] = {0,1,0,-1};
-
-
+#include<vector>
+#include<math.h>
+#include<algorithm>
 using namespace std;
+struct set {
+    int index;
+    int SR, SC, score;
 
-struct santa {
-    int num;
-    int y;
-    int x;
-    int state;
-    int score;
+    bool operator<(const set& d) const {
+        return index < d.index;
+    }
 };
-vector<santa> santas;
 
-bool cmp(santa a, santa b)
-{
-    return a.num < b.num;
-}
-
-int score[31];
-
-int map_[50][50];
+int map[51][51];
+int visit[51][51]; // 산타가 존재하는 곳
 int N, M, P, C, D;
-int sy, sx;
+int Rr, Rc;       // 루돌프 위치
+vector<set> S; // 산타 위치
+int FailSanta = 0;
+int dx[8] = { 1, 0, -1, 0, 1, -1, 1, -1 };
+int dy[8] = { 0, 1, 0, -1, 1, -1, -1, 1 };
+int dx2[4] = { 0, 1, 0, -1 }; //상, 우, 하, 좌
+int dy2[4] = { -1, 0, 1, 0 };
 
-//move((mindir + 2) % 4, ry, rx, santas[i].num);
-                    
+pair<bool, int>S_flag[31];
 
-void move(int dir, int y, int x,int sannum,int type) //산타 재귀로 움직이기
-{
-   
-    int temp = sannum;
-    int ny, nx;
-    if (type == 1)
-    {
-         ny = y + sdy[dir];
-         nx = x + sdx[dir];
-    }
-    else
-    {
-         ny = y + dy[dir];
-         nx = x + dx[dir];
-    }
-
-    
-
-    if (ny<0 || nx<0 || nx>N - 1 || ny>N - 1) //장외
-    {
-        
-        for (int i = 0; i < santas.size(); i++)
-        {
-            if (santas[i].num == map_[y][x])
-            {
-                score[santas[i].num] = santas[i].score;
-                santas.erase(santas.begin() + i);
-            }
+void Interface(int x, int y, int DX, int DY) {
+    int X = x; int Y = y;
+    if (visit[Y][X] != 0) {
+        if (S[visit[Y][X] - 1].SR == 0 && S[visit[Y][X] - 1].SC == 0) return;
+        X = X + DX;  Y = Y + DY;
+        if (X < 1 || Y < 1 || X > N || Y > N) {
+            S[visit[Y - DY][X - DX] - 1].SC = 0; S[visit[Y - DY][X - DX] - 1].SR = 0;
+            FailSanta++;
+            if (FailSanta == P) return;
+            return;
+        }
+        S[visit[Y - DY][X - DX] - 1].SR += DY;
+        S[visit[Y - DY][X - DX] - 1].SC += DX;
+        visit[Y][X] = S[visit[Y - DY][X - DX] - 1].index;
+        if (visit[Y][X] != 0 && visit[Y][X] != visit[Y - DY][X - DX]) {
+            Interface(X, Y, DX, DY);
         }
     }
-    else
-    {
-        if (map_[ny][nx] > 0) //상호작용 일어날경우
-        {
-            move(dir, ny, nx, map_[y][x],type);
-        }
-        else //상호작용 안일어날경우
-        {
-            map_[ny][nx] = map_[y][x];
-        }
-    }
-
-    for (int i = 0; i < santas.size(); i++)
-    {
-        if (santas[i].num == map_[y][x])
-        {
-            santas[i].y = ny;
-            santas[i].x = nx;
-        }
-    }
-
-    map_[y][x] = sannum;
-
 }
 
+void Start(int m) {
+    if (FailSanta == P) return;
+    //기절 풀기
+    if (m > 1) {
+        for (int i = 0; i < S.size(); i++) {
+            if (S[i].SC == 0 && S[i].SR == 0) { continue; } // 탈락한 산타
+            else {
+                S[i].score++;
+            }
+            if (S_flag[i].first && m >= S_flag[i].second + 2) {
+                S_flag[i].first = false;
+                S_flag[i].second = 0;
+            }
 
-int main()
-{
-    cin >> N >> M >> P >> C >> D;
-
-    cin >> sy >> sx;
+        }
+    }
+    if (m > M) return;
 
 
-    sy--;
-    sx--;
-    int ssy = sy;
-    int ssx = sx;
-    map_[sy][sx] = -1;
+    int Min = 2501; // 거리 최소
+    int Sr = 0; int Sc = 0;
+    int Rdx = 0; int Rdy = 0; // 루돌푸 이동 방향
 
-    int num, y, x;
-    for (int i = 0; i < P; i++)
-    {
-        scanf("%d %d %d", &num, &y, &x);
-        y--;
-        x--;
-        santas.push_back({ num,y,x });
-        map_[y][x] = num;
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i].SC == 0 && S[i].SR == 0) continue; // 탈락한 산타
+        int temp = pow((S[i].SR - Rr), 2) + pow((S[i].SC - Rc), 2);
+        if (temp <= Min) {
+            Min = temp;
+        }
     }
 
-    sort(santas.begin(), santas.end(), cmp);
-
-    //반복 시작 //santas size가 0이거나 m번 반복하면 종료
-
-    for (int qwe = 0; qwe < M; qwe++)
-    {
-        if (santas.size() == 0) break;
-        for (int i = 0; i < santas.size(); i++)
-        {
-            if (santas[i].state > 0) santas[i].state--;
-        }
-
-
-
-
-        //루돌프 최소 거리 찾기
-        int mindis = 987654321;
-        santa minsanta;
-        int tempdis;
-        for (int i = 0; i < santas.size(); i++)
-        {
-            tempdis = (sx - santas[i].x) * (sx - santas[i].x) + (sy - santas[i].y) * (sy - santas[i].y);
-            if (mindis > tempdis)
-            {
-                mindis = tempdis;
-                minsanta = santas[i];
+    for (int i = 0; i < S.size(); i++) {
+        int temp = pow((S[i].SR - Rr), 2) + pow((S[i].SC - Rc), 2);
+        if (Min == temp) {
+            if (Sr < S[i].SR) {
+                Sr = S[i].SR;
+                Sc = S[i].SC;
+                //Rdx = dx[k]; Rdy = dy[k];
             }
-            else if (mindis == tempdis)
-            {
-
-                if (santas[i].y > minsanta.y)
-                {
-                    mindis = tempdis;
-                    minsanta = santas[i];
-                }
-                else if (santas[i].y == minsanta.y)
-                {
-                    if (santas[i].x > minsanta.x)
-                    {
-                        mindis = tempdis;
-                        minsanta = santas[i];
-                    }
+            else if (Sr == S[i].SR) {
+                if (Sc < S[i].SC) {
+                    Sr = S[i].SR;
+                    Sc = S[i].SC;
+                    //Rdx = dx[k]; Rdy = dy[k];
                 }
             }
         }
-        printf("\n");
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                printf("%d ", map_[i][j]);
-            }
-            printf("\n");
+    }
+
+    Min = 2501;
+    for (int k = 0; k < 8; k++) {
+        int X = Rc + dx[k]; int Y = Rr + dy[k];
+        int temp = pow((Sr - Y), 2) + pow((Sc - X), 2);
+        if (temp < Min) {
+            Min = temp;
+            Rdx = dx[k]; Rdy = dy[k];
         }
-        
-        //루돌프 이동
+    }
 
-        map_[sy][sx] = 0;
-        
-        mindis = 987654321;
-        int mindir;
-        for (int i = 0; i < 8; i++)
-        {
+    Rr += Rdy; Rc += Rdx; // 루돌푸 이동
 
-            int ny = sy + dy[i];
-            int nx = sx + dx[i];
-            if (ny<0 || nx<0 || nx>N - 1 || ny>N - 1) continue;
-
-            tempdis = (nx - minsanta.x) * (nx - minsanta.x) + (ny - minsanta.y) * (ny - minsanta.y);
-            if (mindis > tempdis)
-            {
-                mindis = tempdis;
-                mindir = i;
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i].SC == 0 && S[i].SR == 0) continue; // 탈락한 산타
+        if (Rr == S[i].SR && Rc == S[i].SC) { // 루돌푸 -> 산타 충돌
+            S[i].score += C;
+            int X = S[i].SC + (Rdx * C); int Y = S[i].SR + (Rdy * C);
+            if (X < 1 || Y < 1 || X > N || Y > N) { // 탈락
+                visit[S[i].SR][S[i].SC] = 0;
+                S[i].SC = 0; S[i].SR = 0;
+                FailSanta++;
+                if (FailSanta == P) return;
             }
-
-        }
-        sy = sy + dy[mindir];
-        sx = sx + dx[mindir];
-
-        //cout << mindir << sy << sx << endl;
-        int ry;
-        int rx;
-        int sannum;
-        if (map_[sy][sx] > 0) // 루돌프가 산타에 충돌
-        {
-
-            sannum = map_[sy][sx];
-            ry = sy + dy[mindir] * C;
-            rx = sx + dx[mindir] * C;
-
-            for (int i = 0; i < santas.size(); i++)
-            {
-                if (santas[i].num == sannum)
-                {
-                    santas[i].score += C;
-                    santas[i].state = 2;
-                }
-            }
-
-            if (ry<0 || rx<0 || rx>N - 1 || ry>N - 1) //장외
-            {
-                //sannum-1지우기
-
-                for (int i = 0; i < santas.size(); i++)
-                {
-                    if (santas[i].num == sannum) {
-                        score[santas[i].num] = santas[i].score;
-                        santas.erase(santas.begin() + i);
-                    }
-
-                }
-
-            }
-            else
-            {
-                for (int i = 0; i < santas.size(); i++)
-                {
-                    if (santas[i].num == sannum)
-                    {
-                        santas[i].y = ry;
-                        santas[i].x = rx;
-                    }
-                }
-                if (map_[ry][rx] > 0) //상호작용 일어날경우
-                {
-                    
-                    move(mindir, ry, rx, sannum,0);
-                }
-                else //상호작용 안일어날경우
-                {
-                    
-                    map_[ry][rx] = sannum;
-                }
-            }
-        }
-        map_[sy][sx] = -1;
-        /*
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                printf("%d ", map_[i][j]);
-            }
-            printf("\n");
-        }
-
-        for (int i = 0; i < santas.size(); i++)
-        {
-            printf("%d %d", santas[i].num, santas[i].state);
-        }*/
-
-        //map_[sy][sx] == 1;
-
-        //cout << sy << sx << endl;
-        //산타가 루돌프에 가기 시작
-
-        for (int i = 0; i < santas.size(); i++)
-        {
-            mindis = 987654321;
-
-            if (santas[i].state > 0) continue;
-
-            for (int j = 0; j < 4; j++)
-            {
-                int ny = santas[i].y + sdy[j];
-                int nx = santas[i].x + sdx[j];
-
-                if (ny<0 || nx<0 || nx>N - 1 || ny>N - 1) continue;
-
-                if (map_[ny][nx] > 0) continue;
-                int nowdis= (santas[i].x - sx) * (santas[i].x - sx) + (santas[i].y - sy) * (santas[i].y - sy);
-                tempdis = (nx - sx) * (nx - sx) + (ny - sy) * (ny - sy);
-
-                if (nowdis < tempdis) continue;
-
-                if (mindis > tempdis)
-                {
-                    mindis = tempdis;
-                    mindir = j;
-                }
-            }
-            /*
-            printf("\n");
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    printf("%d ", map_[i][j]);
-                }
-                printf("\n");
-            }
-            printf("\n");
-            */
-            if (mindis == 987654321) continue;
-            else if (sy == santas[i].y + sdy[mindir] && sx == santas[i].x + sdx[mindir]) //루돌프 만날경우
-            {
+            else { // 기절
                 
+                Interface(X, Y, Rdx, Rdy);
+                visit[S[i].SR][S[i].SC] = 0;
+                S[i].SC = X; S[i].SR = Y;
+                visit[S[i].SR][S[i].SC] = S[i].index;
 
-                ry = sy + sdy[(mindir + 2) % 4] * D;
-                rx = sx + sdx[(mindir + 2) % 4] * D;
-                santas[i].state = 2;
-                santas[i].score += D;
-                map_[santas[i].y][santas[i].x] = 0;
-
-                if (ry<0 || rx<0 || rx>N - 1 || ry>N - 1) //장외
-                {
-                    //sannum-1지우기
-
-                    score[santas[i].num] = santas[i].score;
-                    santas.erase(santas.begin() + i);
-                    i--;
-                }
-                else
-                {
-
-                    if (map_[ry][rx] > 0) //상호작용 일어날경우
-                    {
-                        santas[i].y = ry;
-                        santas[i].x = rx;
-                        
-
-                        move((mindir + 2) % 4, ry, rx, santas[i].num,1);
-                    }
-                    else //상호작용 안일어날경우
-                    {
-
-                        santas[i].y = ry;
-                        santas[i].x = rx;
-                        map_[ry][rx] = santas[i].num;
-                    }
-                }
+                S_flag[i].first = true;
+                S_flag[i].second = m;
             }
-            else //루돌프 안만날경우
-            {
-
-                map_[santas[i].y][santas[i].x] = 0;
-                santas[i].y = santas[i].y + sdy[mindir];
-                santas[i].x = santas[i].x + sdx[mindir];
-                map_[santas[i].y][santas[i].x] = santas[i].num;
-
-
-            }
-
         }
-        
-        printf("\n");
-        printf("final");
-        printf("\n");
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                printf("%d ", map_[i][j]);
-            }
-            printf("\n");
-        }
-        
-        for (int i = 0; i < santas.size(); i++)
-        {
-            santas[i].score++;
-            printf("%d %d %d %d\n", santas[i].num, santas[i].y, santas[i].x, santas[i].score);
-
-        }
-        
     }
 
-    for (int i = 0; i < santas.size(); i++)
-    {
-        score[santas[i].num] = santas[i].score;
+    //============= 산타 이동 ====================
+
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i].SC == 0 && S[i].SR == 0) continue;
+        if (S_flag[i].first) continue;
+        Min = pow((Rr - S[i].SR), 2) + pow((Rc - S[i].SC), 2); // 원래 산타와 루돌푸 거리
+        int Sdx = 0; int Sdy = 0;
+        for (int k = 0; k < 4; k++) {
+            int X = S[i].SC + dx2[k]; int Y = S[i].SR + dy2[k];
+            if (visit[Y][X] != 0) continue;
+            if (X < 1 || Y < 1 || X > N || Y > N) continue;
+            int temp = pow((Rr - Y), 2) + pow((Rc - X), 2);
+            if (temp < Min) {
+                Min = temp;
+                Sdx = dx2[k]; Sdy = dy2[k];
+            }
+        }
+        //산타 이동
+        visit[S[i].SR][S[i].SC] = 0;
+        S[i].SR += Sdy; S[i].SC += Sdx;
+        visit[S[i].SR][S[i].SC] = S[i].index;
+
+        if (Rr == S[i].SR && Rc == S[i].SC) { // 산타 -> 루돌푸 충돌
+            S[i].score += D;
+            int X = S[i].SC - (Sdx * D); int Y = S[i].SR - (Sdy * D);
+            if (X < 1 || Y < 1 || X > N || Y > N) { // 탈락
+                visit[S[i].SR][S[i].SC] = 0;
+                S[i].SC = 0; S[i].SR = 0;
+                FailSanta++;
+                if (FailSanta == P) return;
+            }
+            else { // 기절
+                //visit[S[i].SR][S[i].SC] = 0;
+                Interface(X, Y, -Sdx, -Sdy);
+                visit[S[i].SR][S[i].SC] = 0;
+                S[i].SC = X; S[i].SR = Y;
+                visit[S[i].SR][S[i].SC] = S[i].index;
+
+                S_flag[i].first = true;
+                S_flag[i].second = m;
+            }
+        }
     }
-    //printf("\n");
-    for (int i = 1; i <= P; i++)
-    {
-        cout << score[i] << " ";
+    Start(m + 1); // 다음 판 시작
+}
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    cin >> N >> M >> P >> C >> D;
+    cin >> Rr >> Rc;
+
+    for (int i = 1; i <= P; i++) {
+        int index, SR, SC;
+        cin >> index >> SR >> SC;
+        visit[SR][SC] = index;
+        S.push_back({ index, SR, SC, 0 });
+    }
+    sort(S.begin(), S.end());
+    Start(1);
+
+    for (int i = 0; i < S.size(); i++) {
+        cout << S[i].score << " ";
     }
 
+    return 0;
 }
