ef9c658570aba3656b62c09b94f9d1aa2ff625e7
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-09 16:40:20 +0900

    Title: 코드트리 오마카세, Time: 6553ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240309/코드트리 오마카세/codetree-omakase.py
+++ 240309/코드트리 오마카세/codetree-omakase.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 from collections import defaultdict
 
@@ -5,51 +6,48 @@ L, Q = map(int,sys.stdin.readline().rstrip().split())
 
 name_chobab =defaultdict(list)
 name_info = {}
+q = []
 for _ in range(Q):
     command = sys.stdin.readline().rstrip().split()
     if command[0] == "100":
         name = command[3]
         t,x = int(command[1]),int(command[2])
-        name_chobab[name].append([t,x])
+        if name in name_info:
+            arrival,place,n = name_info[name]
+            if x>place:
+                out = t + (place+L-x)
+                heapq.heappush(q,(out,name))
+            else:
+                out = t + (place-x)
+                heapq.heappush(q,(out,name))
+        else:
+            name_chobab[name].append([t,x])
     elif command[0] =="200":
         name = command[3]
-        t, x = int(command[1]), int(command[2])
+        arrival, place = int(command[1]), int(command[2])
         n = int(command[4])
-        name_info[name] = [t,x,n]
+        name_info[name] = [arrival,place,n]
+        chobabs = name_chobab[name]
+        for t,x in chobabs:
+            new_x = (x+(arrival-t))%L
+            if new_x>place:
+                out = arrival + (place+L-new_x)
+                heapq.heappush(q,(out,name))
+            else:
+                out = arrival + (place-new_x)
+                heapq.heappush(q, (out,name))
+        name_chobab.pop(name)
+
     elif command[0] == "300":
         time = int(command[1])
         peo_count =0
         cho_count = 0
-        for name, info in list(name_info.items())[:]:
-            chobab = name_chobab[name]
-            arrival_time = info[0]
-            place = info[1]
-            remain_chobab = chobab[:]
-            for i,(t,x) in enumerate(chobab):
-                if info[2] ==0:
-                    break
-                t = int(t)
-                x = int(x)
-                if arrival_time < t: #초밥이 늦게 나옴
-                    next_x = x + (time-t)
-                    other_place = place + L
-                    if x<=place<=next_x or x<=other_place<=next_x: #초밥 먹음
-                        info[2]-=1
-                        remain_chobab.remove(chobab[i])
-                        # print(time, name,chobab[i])
-                else: #초밥이 먼저 나와 있음 #arrivalt_time에서의 위치를 구해야함
-                    x = (x + (arrival_time-t))%L
-                    next_x = x + (time-arrival_time)
-                    other_place = place + L
-                    if x<=place<=next_x or x<=other_place<=next_x: #초밥 먹음
-                        info[2]-=1
-                        remain_chobab.remove(chobab[i])
-                        # print(time, name,chobab[i])
-            name_chobab[name] = remain_chobab
-            if info[2] > 0:
-                peo_count +=1
-            if info[2] ==0:
-                name_info.pop(name)
-        for name,cho in name_chobab.items():
-            cho_count+= len(cho)
-        print(peo_count,cho_count)
+        while q and q[0][0] <= time:
+            _,name = heapq.heappop(q)
+            name_info[name][2]-=1
+        for name, info in name_info.items():
+            if info[2] >0:
+                peo_count+=1
+        for name in name_chobab:
+            cho_count+=len(name_chobab[name])
+        print(peo_count,len(q)+cho_count)
