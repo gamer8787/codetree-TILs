cb5b98abc1b32bd9ef87a68dc49ba4569173f26a
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-10 14:41:26 +0900

    Title: 토끼와 경주, Time: 9581ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240310/토끼와 경주/rabit-and-race.py
@@ -0,0 +1,68 @@
+import heapq
+from collections import defaultdict
+
+Q = int(input())
+
+c = list(map(int,input().split()))
+N,M,P = c[1],c[2],c[3]
+rabbit_distance = {}
+rabbit_score={}
+for i in range(4,len(c),2):
+    rabbit_distance[c[i]] = c[i+1]
+    rabbit_score[c[i]] = 0
+rabbit = []
+for key in rabbit_distance:
+    heapq.heappush(rabbit,(0,2,1,1,key))
+for _ in range(Q-2):
+    c = list(map(int,input().split()))
+    if c[0] == 200:
+        K,S = c[1],c[2]
+        turn_rabbit = []
+        for stage in range(1,K+1):
+            # print(stage)
+            avail = []
+
+            jump,sij,i,j,pid = heapq.heappop(rabbit)
+            # print(i,j,pid)
+            d = rabbit_distance[pid]
+            ############################
+            #1 오른쪽
+            nj = (j + d)%(2*M-2)
+            if nj >M:
+                nj = 2*M - nj
+            avail.append([nj+i,i,nj])
+            #2 아래
+            ni = (i + d) % (2 * N - 2)
+            if ni > N:
+                ni = 2 * N - ni
+            avail.append([ni + j, ni, j])
+            #3 왼쪽
+            nj = (j - d)%(2*M-2)
+            if nj >M:
+                nj = 2*M - nj
+            avail.append([nj + i, i, nj])
+            #4 위
+            ni = (i - d) % (2 * N - 2)
+            if ni > N:
+                ni = 2 * N - ni
+            avail.append([ni + j, ni, j])
+            #############################
+            avail.sort(key = lambda x:(-x[0],-x[1],-x[2]))
+            _,new_i,new_j = avail[0]
+            heapq.heappush(rabbit,(jump+1,new_i+new_j,new_i,new_j,pid))
+            # print(new_i, new_j, pid)
+            heapq.heappush(turn_rabbit,(-(new_i+new_j),-new_i,-new_j,-pid))
+            for p in rabbit_score:
+                if p==pid:
+                    continue
+                rabbit_score[p] += new_i+new_j
+        first_rabbit = heapq.heappop(turn_rabbit)
+        pid = -first_rabbit[3]
+        rabbit_score[pid]+=S
+    elif c[0] == 300:
+        pid,L = c[1],c[2]
+        rabbit_distance[pid] *=L
+maximum = 0
+for p in rabbit_score:
+    maximum = max(rabbit_score[p],maximum)
+print(maximum)
